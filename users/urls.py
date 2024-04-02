a4545fe0ab1237551b211b21ff2f9cea1a68b1ce
sarkadol <sarka.dolezalova123@gmail.com>
2024-04-02 12:33:32 +0200

    forgotten password reseforgotten password reset

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
 from . import views
+#from .views import MyLoginView, RegisterView
+
+from django.contrib.auth.views import (
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+
 
 urlpatterns = [
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
-    path('register/', views.sign_up, name='register')
+    path('register/', views.sign_up, name='register'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'),name='password-reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+
 ]
+
+
--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from .forms import LoginForm,RegisterForm
 def sign_up(request):
     if request.method == 'GET':
         form = RegisterForm()
-        return render(request, 'register.html', {'form': form})    
+        return render(request, 'users/register.html', {'form': form})    
    
     if request.method == 'POST':
         form = RegisterForm(request.POST) 
@@ -19,13 +19,13 @@ def sign_up(request):
             login(request, user)
             return redirect('manager_home')
         else:
-            return render(request, 'register.html', {'form': form})
+            return render(request, 'users/register.html', {'form': form})
 
 def sign_in(request):
 
     if request.method == 'GET':
         form = LoginForm()
-        return render(request,'login.html', {'form': form})
+        return render(request,'users/login.html', {'form': form})
     
     elif request.method == 'POST':
         form = LoginForm(request.POST)
@@ -41,7 +41,7 @@ def sign_in(request):
         
         # form is not valid or user is not authenticated
         messages.error(request,f' Invalid username or password')
-        return render(request,'login.html',{'form': form})
+        return render(request,'users/login.html',{'form': form})
     
 def sign_out(request):
     logout(request)
