36551ef2036d2a2a4e12d8ac6c5f85d26811665a
sarkadol <sarka.dolezalova123@gmail.com>
2024-04-03 18:31:06 +0200

    small changes

--- base/views.py
+++ base/views.py
@@ -257,7 +257,7 @@ def new_reservation_timetable(request, offer_id=None, category_id=None):
                 "hours": hours,
                 "category_name": category.category_name,
                 "category_id": category_id,
-                "unit_id": 2,
+                #"unit_id": 2,
             },
             request=request,
         )
--- quickreserve/settings.py
+++ quickreserve/settings.py
@@ -147,3 +147,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 1800 # 1800 Session will expire in 30 minutes (seconds)
